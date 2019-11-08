fd5a2939496350c99c51ad3d0a2320bbc52f4114
lxy926 <1073008504@qq.com>
2019-11-08 10:12:32 +0800

    init gshop

---
+++ src/pages/Msite/Msite.vue
@@ -0,0 +1,291 @@
+<template>
+  <div class="msite">
+    <!--首页头部-->
+    <HeaderTop title="昌平区北七家宏福科技园(337省道北)">
+      <a class="header_search" slot="left">
+        <i class="iconfont icon-sousuo"></i>
+      </a>
+      <a class="header_login" slot="right">
+        <span class="header_login_text">登录|注册</span>
+      </a>
+    </HeaderTop>
+    <!-- 首页导航 -->
+    <nav class="msite_nav border-1px">
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/1.jpg" />
+                <span>甜品饮品</span>
+              </div>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/2.jpg" />
+                <span>商超便利</span>
+              </div>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/3.jpg" />
+                <span>美食</span>
+              </div>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/4.jpg" />
+                <span>简餐</span>
+              </div>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/5.jpg" />
+                <span>新店特惠</span>
+              </div>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/6.jpg" />
+                <span>准时达</span>
+              </div>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/7.jpg" />
+                <span>预订早餐</span>
+              </div>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/8.jpg" />
+                <span>土豪推荐</span>
+              </div>
+            </a>
+          </div>
+          <div class="swiper-slide">
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/9.jpg" />
+                <span>甜品饮品</span>
+              </div>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/10.jpg" />
+                <span>商超便利</span>
+              </div>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/11.jpg" />
+                <span>美食</span>
+              </div>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/12.jpg" />
+                <span>简餐</span>
+              </div>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/13.jpg" />
+                <span>新店特惠</span>
+              </div>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/14.jpg" />
+                <span>准时达</span>
+              </div>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/1.jpg" />
+                <span>预订早餐</span>
+              </div>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/2.jpg" />
+                <span>土豪推荐</span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <!-- Add Pagination -->
+        <div class="swiper-pagination"></div>
+      </div>
+    </nav>
+    <!--首页附近商家-->
+    <div class="msite_shop_list border-1px">
+      <div class="shop_header">
+        <i class="iconfont icon-xuanxiang"></i>
+        <span class="shop_header_title">附近商家</span>
+      </div>
+      <ShopList></ShopList>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderTop from '../../components/HeaderTop/HeaderTop.vue'
+//引入 swiper
+import Swiper from 'swiper'
+import 'swiper/css/swiper.min.css'
+
+import ShopList from '../../components/ShopList/ShopList.vue'
+
+export default {
+  mounted() {
+    //创建一个swiper对象  来实现轮播
+    new Swiper('.swiper-container',{
+      loop:true, //   循环轮播
+      pagination: { // 分页器
+        el:'.swiper-pagination'
+      }
+    })
+  },
+  components : {
+    HeaderTop,
+    ShopList
+  }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import '../../common/stylus/mixins.styl';
+
+.msite {
+  width: 100%;
+
+  .header {
+    background-color: #02a774;
+    position: fixed;
+    z-index: 100;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 45px;
+
+    .header_search {
+      position: absolute;
+      left: 15px;
+      top: 50%;
+      transform: translateY(-50%);
+      width: 10%;
+      height: 50%;
+
+      .icon-sousuo {
+        font-size: 25px;
+        color: #fff;
+      }
+    }
+
+    .header_title {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 50%;
+      color: #fff;
+      text-align: center;
+
+      .header_title_text {
+        font-size: 20px;
+        color: #fff;
+        display: block;
+      }
+    }
+
+    .header_login {
+      font-size: 14px;
+      color: #fff;
+      position: absolute;
+      right: 15px;
+      top: 50%;
+      transform: translateY(-50%);
+
+      .header_login_text {
+        color: #fff;
+      }
+    }
+  }
+
+  .msite_nav {
+    bottom-border-1px(#e4e4e4);
+    margin-top: 45px;
+    height: 200px;
+    background: #fff;
+
+    .swiper-container {
+      width: 100%;
+      height: 100%;
+
+      .swiper-wrapper {
+        width: 100%;
+        height: 100%;
+
+        .swiper-slide {
+          display: flex;
+          justify-content: center;
+          align-items: flex-start;
+          flex-wrap: wrap;
+
+          .link_to_food {
+            width: 25%;
+
+            .food_container {
+              display: block;
+              width: 100%;
+              text-align: center;
+              padding: 10px;
+              font-size: 0;
+
+              img {
+                display: inline-block;
+                width: 50px;
+                heihgt: 50px;
+              }
+
+              span {
+                display: block;
+                width: 100%;
+                text-align: center;
+                font-size: 13px;
+                color: #666;
+              }
+            }
+          }
+        }
+      }
+
+      .swipre-pagination {
+        >span .swiper-pagination-bullet-active {
+          background: #02a774;
+        }
+      }
+    }
+  }
+
+  .msite_shop_list {
+      top-border-1px(#e4e4e4);
+      margin-top: 10px;
+      background: #ffffff;
+      .shop_header {
+          padding:10px 10px 0px 10px;
+          .shop_icon {
+              margin-left:5px;
+              color:#999;
+          }
+          .shop_header_title {
+              color:#999;
+              font-size:14px;
+              line-height:20px;
+          }
+      }
+      
+  }
+}
+</style>
